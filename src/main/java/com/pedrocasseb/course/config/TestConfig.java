911ff76d3ef059a09f20913a2cdaf442e34243a6
Pedro Casseb <casseb.phcc@gmail.com>
2026-06-17 12:03:36 -0300

    feat(user): add repository and test config

---
+++ src/main/java/com/pedrocasseb/course/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.pedrocasseb.course.config;
+
+import com.pedrocasseb.course.entities.User;
+import com.pedrocasseb.course.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+@RequiredArgsConstructor
+public class TestConfig implements CommandLineRunner {
+    private final UserRepository userRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "98888888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "8999999999", "123456");
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
---
+++ src/main/java/com/pedrocasseb/course/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.pedrocasseb.course.repositories;
+
+import com.pedrocasseb.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
