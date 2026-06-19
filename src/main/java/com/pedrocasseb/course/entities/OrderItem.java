2c9b782890e70ea2678e42e30e75c59b9f0e7729
Pedro Casseb <casseb.phcc@gmail.com>
2026-06-19 14:04:50 -0300

    feat(entities): add OrderItem and OrderItemPk entities

---
+++ src/main/java/com/pedrocasseb/course/entities/OrderItem.java
@@ -0,0 +1,54 @@
+package com.pedrocasseb.course.entities;
+
+import com.pedrocasseb.course.entities.pk.OrderItemPk;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
+    @EqualsAndHashCode.Include
+    @EmbeddedId
+    private OrderItemPk id;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        super();
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+}
---
+++ src/main/java/com/pedrocasseb/course/entities/pk/OrderItemPk.java
@@ -0,0 +1,30 @@
+package com.pedrocasseb.course.entities.pk;
+
+import com.pedrocasseb.course.entities.Order;
+import com.pedrocasseb.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Embeddable
+public class OrderItemPk implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
